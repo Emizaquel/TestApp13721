00981c8da014dae93b7685d744cf118403df57bf
Emizaquel <emizaquel@gmail.com>
2018-11-25 18:20:28 +0000

    test testing possible motion

--- snowstormCode.js
+++ snowstormCode.js
@@ -8,16 +8,16 @@ function snowScript(time, wind){
   // var fTop = elems[0].style.top;
 
   for (var i = 0; i < elems.length; i++) {
-      delSize = Math.random();
-      delxMove = (Math.random()*wind) - (0.3*wind);
-      delyMove = Math.random()*10 - 3;
-      fTop = parsefloat(elems[i].style.top);
-      if(fTop < 0){
-        fTop = fTop + 1;
-      }else if (fTop > 90) {
-        fTop = 0;
-      }
-      elems[i].style.top = (fTop-delyMove) + "%";
+      // delSize = Math.random();
+      // delxMove = (Math.random()*wind) - (0.3*wind);
+      // delyMove = Math.random()*10 - 3;
+      // fTop = parsefloat(elems[i].style.top);
+      // if(fTop < 0){
+      //   fTop = fTop + 1;
+      // }else if (fTop > 90) {
+      //   fTop = 0;
+      // }
+      // elems[i].style.top = (fTop-delyMove) + "%";
       console.log(parsefloat(elems[i].style.top));
   }
 
