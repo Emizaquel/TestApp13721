41814a1d8ab9d76a791fd92341e67567a20e74de
Emizaquel <emizaquel@gmail.com>
2018-11-25 17:37:02 +0000

    testflake selector

--- snowstormCode.js
+++ snowstormCode.js
@@ -1,3 +1,13 @@
-function snowScript(elem){
-  console.log(elem);
+function snowScript(time, wind){
+  var delSize = Math.random();
+  var delxMove = (Math.random()*wind) - (0.3*wind);
+  var delyMove = Math.random()*1 - 0.3;
+
+  var textBoxes = document.querySelectorAll('[id^=flake]');
+  var textToWrite;
+  for(var i in textBoxes){
+     console.log(i);
+  }
+
+  setTimeout(snowScript(time), time);
 }
