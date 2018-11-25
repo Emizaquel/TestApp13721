d0c4411fe2ecb0945c6f002b261721dd206b2cc4
Emizaquel <emizaquel@gmail.com>
2018-11-25 18:01:28 +0000

    test testing possible motion

--- snowstormCode.js
+++ snowstormCode.js
@@ -3,11 +3,13 @@ function snowScript(time, wind){
   var elems = document.getElementsByClassName("flake");
 
   elems.forEach(function(elem){
+  });
+  for (var i = 0; i < elems.length; i++) {
       var delSize = Math.random();
       var delxMove = (Math.random()*wind) - (0.3*wind);
       var delyMove = Math.random()*1 - 0.3;
-      elem.style.top = elem.style.top-delyMove;
-  });
+      elems[i].style.top = elem.style.top-delyMove;
+  }
 
   //setTimeout(snowScript(time), time);
 }
