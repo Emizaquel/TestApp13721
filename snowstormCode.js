40892f71f9b2ea039fd180ab0c385f0d4d1d519d
Emizaquel <emizaquel@gmail.com>
2018-11-25 18:07:25 +0000

    test testing possible motion

--- snowstormCode.js
+++ snowstormCode.js
@@ -2,20 +2,23 @@ function snowScript(time, wind){
 
   var elems = document.getElementsByClassName("flake");
 
-  var delSize = Math.random();
-  var delxMove = (Math.random()*wind) - (0.3*wind);
-  var delyMove = Math.random()*1 - 0.3;
-  var fTop = elems[0].style.top;
+  // var delSize = Math.random();
+  // var delxMove = (Math.random()*wind) - (0.3*wind);
+  // var delyMove = Math.random()*1 - 0.3;
+  // var fTop = elems[0].style.top;
 
   for (var i = 0; i < elems.length; i++) {
-      delSize = Math.random();
-      delxMove = (Math.random()*wind) - (0.3*wind);
-      delyMove = Math.random()*1 - 0.3;
-      fTop = elems[i].style.top;
-      if(fTop < 0){
-        fTop = fTop + 1;
-      }
-      elems[i].style.top = (fTop-delyMove) + "%";
+      // delSize = Math.random();
+      // delxMove = (Math.random()*wind) - (0.3*wind);
+      // delyMove = Math.random()*10 - 3;
+      // fTop = elems[i].style.top;
+      // if(fTop < 0){
+      //   fTop = fTop + 1;
+      // }else if (fTop > 90) {
+      //   fTop = 0;
+      // }
+      // elems[i].style.top = (fTop-delyMove) + "%";
+      console.log(elems[i]);
   }
 
   //setTimeout(snowScript(time), time);
